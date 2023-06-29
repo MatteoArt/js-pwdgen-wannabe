75c38621fcd43306e81cdbb16fcff31e90cfaa5e
MatteoArt <mattrim8@gmail.com>
2023-06-29 17:22:02 +0200

    completo bonus

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const userColor = prompt("Quale'è il tuo colore preferito?");
 const age = prompt("Quanti anni hai?");
 
 //numero che verrà stampato nella password
-
+const pwdNumber = parseInt(age) + 21;
 //console.log(userName, surname, userColor, age);
 
-document.getElementById("pwd").innerHTML = `${userName}${surname}${userColor}21`;
+document.getElementById("pwd").innerHTML = `${userName}${surname}${userColor}${pwdNumber}`;
