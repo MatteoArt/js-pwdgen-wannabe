fc1f2ca691b9d07b4238e179cb94100bdd4e19d7
MatteoArt <mattrim8@gmail.com>
2023-06-29 17:04:42 +0200

    completo esercizio base

--- js/main.js
+++ js/main.js
@@ -3,4 +3,8 @@ const surname = prompt("Inserisci il tuo cognome");
 const userColor = prompt("Quale'è il tuo colore preferito?");
 const age = prompt("Quanti anni hai?");
 
-console.log(userName, surname, userColor, age);
+//numero che verrà stampato nella password
+
+//console.log(userName, surname, userColor, age);
+
+document.getElementById("pwd").innerHTML = `${userName}${surname}${userColor}21`;
