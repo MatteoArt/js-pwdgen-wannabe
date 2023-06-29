4cd3d4c4d469cafba2072a1cecffd7407cc7b046
MatteoArt <mattrim8@gmail.com>
2023-06-29 16:42:40 +0200

    raccolgo i vari input inseriti dall'utente

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const userName = prompt("Inserisci il tuo nome");
+const surname = prompt("Inserisci il tuo cognome");
+const userColor = prompt("Quale'è il tuo colore preferito?");
+const age = prompt("Quanti anni hai?");
+
+console.log(userName, surname, userColor, age);
